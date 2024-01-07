ee49d69161adc5459988cade2610951b69ce657d
StenniHub <91371343+StenniHub@users.noreply.github.com>
2024-01-07 22:04:30 +0100

    Use seed to randomize stacked condition for each tile

--- BingoStack.js
+++ BingoStack.js
@@ -1,10 +1,15 @@
 var currentSeed = null;
+const weaponCategories = ["Daggers","Straight Swords","Greatswords","Ultra Greatswords","Curved Swords","Katanas","Curved Greatswords","Piercing Swords","Axes","Great Axes","Hammers","Great Hammers", "Fist & Claw", "Spears","Halberds","Lances","Scythes","Twinblades","Whips","Bows","Greatbows","Crossbows","Pyro","Faith","Sorcery","Boss Weapons"];
 
 function stackItUp() {
     console.log("Let's go stack it up!")
+    const numberGenerator = new RNG(parseInt(currentSeed));
+
     const tiles = document.getElementsByClassName("text-container");
     for (tile of tiles) {
-        tile.textContent += " with a dagger";
+        const conditionIdx = numberGenerator.nextRange(0, weaponCategories.length);
+        const condition = weaponCategories[conditionIdx];
+        tile.textContent += " with " + condition;
     }
 }
 
@@ -23,21 +28,19 @@ setInterval(checkIfSeedChanged, 1000);
 
 // document.getElementById("stackButton").addEventListener("click", stackItUp);
 
-
 // FIXME: WHY IS NO WORK???
 /*
-const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutationRecord) {
-        console.log(mutationRecord);
-    });
-});
+const Observe = (sel, opt, cb) => {
+    const Obs = new MutationObserver((m) => [...m].forEach(cb));
+    document.querySelectorAll(sel).forEach(el => Obs.observe(el, opt));
+};
 
-const theSeed = document.getElementById("the-seed");
-console.log(theSeed.textContent);
-observer.observe(theSeed, {
-    attributes: true,
+Observe("#the-seed", {
+    characterData: false,
+    attributes: false,
     childList: true,
-    characterData: true,
-    subtree: true
+    subtree: false
+}, (m) => {
+    console.log(m.target.textContent);
 });
 */
